f1ef7b714259edffe5f644e81f6b290a582d39e3
Barikaa <48105084+Barikaa@users.noreply.github.com>
2019-11-28 14:41:17 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "NotificationView",
+    platforms: [
+       .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "NotificationView",
+            targets: ["NotificationView"]),
+    ],
+    dependencies: [
+
+    ],
+    targets: [
+        .target(
+            name: "NotificationView",
+            dependencies: [],
+            path: "./NotificationView"),
+    ]
+)
